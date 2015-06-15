7fe44968c80dafccc30bdb692bb74b71d5f77be2
Heng Li <liheng620@gmail.com>
2015-06-15 12:53:33 -0700

    copy file to hdfs

---
+++ hia/src/hia/PutMerge.java
@@ -0,0 +1,46 @@
+package hia;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class PutMerge {
+	public static void main(String[] args) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem hdfs = FileSystem.get(conf);
+		FileSystem local = FileSystem.getLocal(conf);
+		Path inputDir = new Path(args[0]);
+		Path hdfsFile = new Path(args[1]);
+		FSDataInputStream in = null;
+		FSDataOutputStream out = null;
+		try {
+			FileStatus[] inputFiles = local.listStatus(inputDir);
+			out = hdfs.create(hdfsFile);
+			for (int i = 0; i < inputFiles.length; i++) {
+				System.out.println(inputFiles[i].getPath().getName());
+				in = local.open(inputFiles[i].getPath());
+				byte buffer[] = new byte[256];
+				int bytesRead = 0;
+				while ((bytesRead = in.read(buffer)) > 0) {
+					out.write(buffer, 0, bytesRead);
+				}
+				in.close();
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+}
