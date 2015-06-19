5df2975af780b3a522ed3a6e8122223469f2a639
Heng Li <liheng620@gmail.com>
2015-06-19 12:09:56 -0700

    Custome TimeUrlLIneRecordReader

---
+++ hia/src/hia/URLWritable.java
@@ -0,0 +1,34 @@
+package hia;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.hadoop.io.Writable;
+
+public class URLWritable implements Writable {
+	protected URL url;
+
+	public URLWritable() {
+	}
+
+	public URLWritable(URL url) {
+		this.url = url;
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		url = new URL(in.readUTF());
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeUTF(url.toString());
+	}
+	
+	public void set(String s) throws MalformedURLException {
+		url = new URL(s);
+	}
+}
