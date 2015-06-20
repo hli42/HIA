049dfc99041acdcbc444d5e999455ef483663f1c
Heng Li <liheng620@gmail.com>
2015-06-19 17:26:27 -0700

    CitationHistogram.java done

--- hia/src/patent/CitationHistogram.java
+++ hia/src/patent/CitationHistogram.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
@@ -21,49 +22,51 @@ import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class MyJob extends Configured implements Tool {
-	public static class MapClass extends MapReduceBase implements Mapper<Text, Text, Text, Text> {
+public class CitationHistogram extends Configured implements Tool {
+	public static class MapClass extends MapReduceBase implements Mapper<Text, Text, IntWritable, IntWritable> {
+		private final static IntWritable uno = new IntWritable(1);
+		private IntWritable citationCount = new IntWritable();
+
 		@Override
-		public void map(Text key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-			output.collect(value, key);
+		public void map(Text key, Text value, OutputCollector<IntWritable, IntWritable> output, Reporter reporter) throws IOException {
+			citationCount.set(Integer.parseInt(value.toString()));
+			output.collect(citationCount, uno);
+		}
+	}
+
+	public static class ReduceClass extends MapReduceBase implements Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+		@Override
+		public void reduce(IntWritable key, Iterator<IntWritable> values, OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
+				throws IOException {
+			int count = 0;
+			while (values.hasNext()) {
+				count += values.next().get();
+			}
+			output.collect(key, new IntWritable(count));
 		}
 	}
 
 	@Override
 	public int run(String[] args) throws Exception {
 		Configuration conf = getConf();
-		JobConf job = new JobConf(conf, MyJob.class);
+		JobConf job = new JobConf(conf, CitationHistogram.class);
 		Path in = new Path(args[0]);
 		Path out = new Path(args[1]);
 		FileInputFormat.setInputPaths(job, in);
 		FileOutputFormat.setOutputPath(job, out);
-		job.setJobName("My Job");
+		job.setJobName("Citation Histogram Job");
 		job.setMapperClass(MapClass.class);
 		job.setReducerClass(ReduceClass.class);
 		job.setInputFormat(KeyValueTextInputFormat.class);
 		job.setOutputFormat(TextOutputFormat.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(IntWritable.class);
 		JobClient.runJob(job);
 		return 0;
 	}
 
-	public static class ReduceClass extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-		@Override
-		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-			String csv = "";
-			while (values.hasNext()) {
-				if (csv.length() > 0) {
-					csv += ",";
-				}
-				csv += values.next().toString();
-			}
-			output.collect(key, new Text(csv));
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new Configuration(), new MyJob(), args);
+		int res = ToolRunner.run(new Configuration(), new CitationHistogram(), args);
 		System.exit(res);
 	}
 }
